e7df48446691459e0ffcc4bae9de1087544b3485
user3345 <81963672+genan2003@users.noreply.github.com>
2025-07-26 15:57:16 +0300

    Add the pins module.

---
+++ src/pins.rs
@@ -0,0 +1,25 @@
+//Digital Pins
+pub type D0 = PA3;
+pub type D1 = PA2;
+pub type D2 = PC8;
+pub type D3 = PB3;
+pub type D4 = PB5;
+pub type D5 = PB4;
+pub type D6 = PB10;
+pub type D7 = PA8;
+pub type D8 = PC7;
+pub type D9 = PC6;
+pub type D10 = PC9;
+pub type D11 = PA7;
+pub type D12 = PA6;
+pub type D13 = PA5;
+pub type D14 = PB7;
+pub type D15 = PB6;
+
+//Analog Pins
+pub type A0 = PA0;
+pub type A0 = PA1;
+pub type A0 = PA4;
+pub type A0 = PB0;
+pub type A0 = PC1;
+pub type A0 = PC0;
