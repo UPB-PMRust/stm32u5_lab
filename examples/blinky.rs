c7b945a55f43f0b825d5e7228b850341c4957fbf
genan2003 <genanomer99@gmail.com>
2025-07-30 21:57:21 +0300

    Update the main.rs and examples/blink.rs

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,19 +1,23 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+
+mod nucleo_u545re_q; // Import the correct board module.
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    
-    let mut led = Output::new(p.PA5, Level::Low, Speed::Medium); //The builtin LED of the board(LD2) is on the pin PA5.
+    let d13_pin: nucleo_u545re_q::D13 = p.PA5; // Adjust this pin according to your board's LED pin
+
+    // replace PC13 with the right pin for your board.
+    let mut led = Output::new(d13_pin, Level::Low, Speed::Medium);
 
     loop {
         defmt::info!("on!");
