dec369030e880c8af0ebc428ac3b6f8b700d2b29
genan2003 <genanomer99@gmail.com>
2025-07-31 01:16:34 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,18 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::Timer;
-use {defmt_rtt as _, panic_probe as _};
+use embassy_stm32::{self as _, Config};
 
-mod nucleo_u545re_q;
+use defmt_rtt as _;
+use embassy_time::Timer;
+use panic_probe as _;
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) -> ! {
-    let p = embassy_stm32::init(Default::default());
-
-    let d13_pin: nucleo_u545re_q::D13 = p.PA5; // Adjust this pin according to your board's LED pin
-
-    let mut led = Output::new(d13_pin, Level::Low, Speed::Medium); //The builtin LED of the board(LD2) is on the pin PA5.
-
+async fn main(_spawner: Spawner) {
+    let config = Config::default();
+    let _peripherals = embassy_stm32::init(config);
     loop {
-        defmt::info!("on!");
-        led.set_low();
-        Timer::after_millis(200).await;
-
-        defmt::info!("off!");
-        led.set_high();
-        Timer::after_millis(200).await;
+        info!("Hello");
+        Timer::after_secs(1).await;
     }
 }
